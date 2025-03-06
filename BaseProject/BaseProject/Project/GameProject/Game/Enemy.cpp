723ae2288c2009bd797ba0109800355597be3622
矢 <24051910@d.anabuki-college.net>
2025-03-06 11:22:00 +0900

    Enemyのヘッダーなどの追加

---
+++ BaseProject/BaseProject/Project/GameProject/Game/Enemy.cpp
@@ -0,0 +1,21 @@
+#include "Enemy.h"
+
+#define CHIP_SIZE 24 
+#define CENTER_POS CVector2D(12.0f,12.0f) 
+#define MOVE_SPEED_X 3.0f 
+#define MOVE_SPEED_Y 3.0f 
+Enemy::Enemy(const CVector2D& pos)
+{
+}
+
+Enemy::~Enemy()
+{
+}
+
+void Enemy::Update()
+{
+}
+
+void Enemy::Render()
+{
+}
