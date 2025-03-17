f2fa9edc50024c39916470918d96d0819e59d715
fuka <fuka.bigkatsu0531@icloud.com>
2025-03-17 14:54:23 +0900

    更新

--- BaseProject/BaseProject/Project/GameProject/Game/Field.cpp
+++ BaseProject/BaseProject/Project/GameProject/Game/Field.cpp
@@ -5,15 +5,18 @@ Field::Field(const CVector3D& pos)
 {
 	m_backGround = COPY_RESOURCE("BackGround", CImage);
 	m_ground = COPY_RESOURCE("Ground", CImage);
+	m_backgroundscroll = 0;
 }
 void Field::Update()
 {
-
+	m_backgroundscroll += 0.5;
 }
 
 void Field::Render()
 {
 	float sc;
+	sc = m_backgroundscroll;
+	m_backGround.SetRect(sc, 0, sc + 1920, 1080);
 	m_backGround.Draw();
 	
 	sc = ObjectBase::m_scroll.x/3;
